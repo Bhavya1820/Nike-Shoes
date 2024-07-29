a62a8e513a12c87433549723e1ebaca598e0feeb
Bhavya1820 <bhavyasoni1820@gmail.com>
2024-07-29 17:39:55 +0530

    Nike
    Nike-Brand-Page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import Navigation from "./components/navigation";
+
+const App = () => {
+  return(
+    <div>
+      <Navigation></Navigation>
+      <HeroSection></HeroSection>
+    </div>
+  )
+}
+
+export default App;
